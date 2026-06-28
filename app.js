8d7054cdea2e9d38ff2240b1d21739ea43326c7f
Sergio <sergiogimenez060705@gmail.com>
2026-06-29 00:22:32 +0200

    Cambio

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function mostrarInicio() {
     const app = document.getElementById('app');
     app.innerHTML = `
         <div class="pantalla inicio">
-            <div class="header"><h1>🍷 Bar La Bodegaaaa</h1></div>
+            <div class="header"><h1>🍷 Bar La Bodega</h1></div>
             <p class="subtitulo">Selecciona un salón</p>
             <div class="salones-grid" id="salonesGrid"></div>
             <div class="botones-flotantes"><button class="btn-secundario" onclick="mostrarHistorial()">📋 Historial</button></div>
